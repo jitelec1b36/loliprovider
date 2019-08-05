e0eac00c4d3fb2af918be0902346ced990846ed1
jitelec1b36 <rinshi246@gmail.com>
2019-08-05 19:51:27 +0800

    Update loli provider.py

--- loli provider.py
+++ loli provider.py
@@ -159,4 +159,4 @@ client.loop.create_task(background_loop_futa())
 client.loop.create_task(background_loop_monster())
 client.loop.create_task(background_loop_lewd())
 
-bot.run('NjA3NDc3NDE5MzkzNDE3MjI3.XUgXyA.wZFAY33sC4oTQsGPrkWRmCD6KvU')
+bot.run('')
